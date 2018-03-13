202cc3964fcd8fc8d12222c7596b82e71ad31279
firsticz <thanawat6368@gmail.com>
2018-03-13 12:26:06 +0700

    fix js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
 
     $('#login-form-link').click(function(e) {
@@ -14,25 +13,5 @@ $(function() {
 		$('#login-form-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
-	});
-
-=======
-$(function() {
-
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-
->>>>>>> 54949563721c814711ed8d1874bd44f706698bf3
+    });
 });
